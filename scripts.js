9c4ec3f9859bdfeb6caf436fc2db32bd9214c205
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-11-15 16:46:01 -0300

    Add files via upload
    starting with the js functionality to read the json file.

--- scripts.js
+++ scripts.js
@@ -2,4 +2,39 @@ console.log("from js");
 
 fetch('data.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => { 
+        console.log(json);
+        getUser(json);
+        console.log("hola ", user.username);
+        console.log("tu imagen es : ", user.image);
+        const comment = getComment(json, 0);
+        console.log(comment != null ? comment.content : "no encontrado");
+    });
+
+let user = {
+    "image" : null,
+    "username" : null
+}
+let comment = {
+    "id" : null,
+    "content": null,
+    "createdAt": null,
+    "score": null,
+    "user": null,
+    "replies": null
+}
+    
+const getUser = (json) => {
+    user.image = json.currentUser.image.png;
+    user.username = json.currentUser.username;
+}
+
+const getComment = (json, id) => {
+    if (json.comments[id] != null){
+        console.log("encontrado");
+        return json.comments[id];
+    }else{
+        console.log("no encontrado");
+        return null;
+    }    
+}
